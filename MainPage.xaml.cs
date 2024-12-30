558954014f77251921c21e895eb8737e3deb43dc
alpayclsr <alpay665@hotmail.com>
2024-12-30 14:20:51 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace WebViewReproductionApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent(); 
+
+            // Problem occur because of "&  => %26"
+            webView.Source = "https://github.com/Pelinalpp/TestFiles/blob/main/01-A%C4%9F-Sistem%20Bilgi%20G%C3%BCvenli%C4%9Fi%20Md/Ortak%20Rehberler/Change%20Password%20Guide.pdf";
+
+
+        }
+    }
+    
+
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WebViewReproductionApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
